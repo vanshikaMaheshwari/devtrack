46ebf86c65294edea5efbca85f325aaf600319ca
abdullahxyz85 <abdullahxyz85@gmail.com>
2026-05-19 20:00:05 +0500

    fix: correct SVG arrow direction in back to top button - change from down arrow to up arrow

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -52,8 +52,7 @@ export default function BackToTopButton() {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M7 16l-4-4m0 0l4-4m-4 4h18"
-              transform="rotate(90 12 12)"
+              d="M5 15l7-7 7 7"
             />
           </svg>
         </button>
